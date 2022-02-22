018e33d7c6d80ae05247a2afc270e71473d78339
Michał Liebner <liebner15@gmail.com>
2022-02-22 21:47:00 +0100

    updated cli

--- bin/cli.js
+++ bin/cli.js
@@ -4,33 +4,35 @@ const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: 'inherit' });
   } catch (e) {
-    console.error(`Failed to execute ${command}`, e);
+    console.error(`☠️❌ Failed to execute ${command}`, e);
     return false;
   }
   return true;
 };
 const repoName = process.argv[2];
 if (!repoName) {
-  console.log('☠️ Please add name for repository');
+  console.log('☠️❌ Please add name for repository');
   process.exit(-1);
 }
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/liebner12/create-next-starter ${repoName}`;
 const installDepsCommand = `cd ${repoName} && yarn`;
 
-console.log(`🤖Cloning the repository with name ${repoName}`);
+console.log(`🤖 Cloning the repository with name ${repoName}`);
 
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) {
-  console.log('🤯 Error occured while clonning');
+  console.log('☠️❌ Error occured while clonning');
   process.exit(-1);
 }
 
 console.log(`🛠️ Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) {
-  console.log('🤯 Error occured while installing');
+  console.log('☠️❌ Error occured while installing ');
   process.exit(-1);
 }
 
-console.log('🎊🎉 Congratulations! Follow the following command to start');
+console.log(
+  '🎉🎉🎉 Congratulations! Follow the following command to start 🎉🎉🎉'
+);
 console.log(`cd ${repoName} && npm start`);
