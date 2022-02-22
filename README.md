44eda4e504b25ea464b72a616a9e6dd7e37bbc3b
Michał Liebner <liebner15@gmail.com>
2022-02-22 21:31:17 +0100

    update

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 ## Getting Started
 
+Run with:
+
+```bash
+npx create-next-ts-starter
+```
+
+It contains default setup for projects.
+Main technologies used:
+
+- React
+- Next.js
+- Typescript
+- Tailwind.css
+- Docker
+- NextPWA
+- Sass
+
+All the config for eslint, prettier, husky and typescript is provided.
+
 Avalible commands:
 
 ```bash
---
+++ bin/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+const repoName = process.argv[2];
+if (!repoName) {
+  console.log('☠️ Please add name for repository');
+  process.exit(-1);
+}
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/liebner12/create-next-starter ${repoName}`;
+const installDepsCommand = `cd ${repoName} && yarn`;
+
+console.log(`🤖Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) {
+  console.log('🤯 Error occured while clonning');
+  process.exit(-1);
+}
+
+console.log(`🛠️ Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) {
+  console.log('🤯 Error occured while installing');
+  process.exit(-1);
+}
+
+console.log('🎊🎉 Congratulations! Follow the following command to start');
+console.log(`cd ${repoName} && npm start`);
