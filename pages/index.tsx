ecdb8b8611453361d5e0549c8f4edb5722c96e06
Michał Liebner <liebner15@gmail.com>
2022-02-28 19:45:30 +0100

    feat: adds components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import HelloWorld from '../components';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
 
       <main>
         <h1>Hello world!</h1>
+        <HelloWorld />
       </main>
     </div>
   );
