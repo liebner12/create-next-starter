bb532e1d9a084f6b22468cd80205d6d114f66e1e
Michał Liebner <liebner15@gmail.com>
2022-03-02 23:19:30 +0100

    fix: working example
    working starter

--- bin/cli.js
+++ bin/cli.js
@@ -4,31 +4,31 @@ const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: 'inherit' });
   } catch (e) {
-    console.error(`☠️❌ Failed to execute ${command}`, e);
+    console.error(`❌ Failed to execute ${command} ❌`, e);
     return false;
   }
   return true;
 };
 const repoName = process.argv[2];
 if (!repoName) {
-  console.log('☠️❌ Please add name for repository');
+  console.log('❌ Please add name for repository ❌');
   process.exit(-1);
 }
-const gitCheckoutCommand = `git clone https://github.com/liebner12/create-next-starter-repo ${repoName}; cd ${repoName}; rm -r -fo .git;`;
+const gitCheckoutCommand = `git clone https://github.com/liebner12/create-next-starter-repo ${repoName} && cd ${repoName} && npx rimraf .git`;
 const installDepsCommand = `yarn`;
-console.log(gitCheckoutCommand);
 console.log(`🤖 Cloning the repository with name ${repoName}`);
 
 const checkedOut = runCommand(gitCheckoutCommand);
+
 if (!checkedOut) {
-  console.log('☠️❌ Error occured while clonning');
+  console.log('❌ Error occured while clonning ❌');
   process.exit(-1);
 }
 
-console.log(`🛠️ Installing dependencies for ${repoName}`);
+console.log(`🔨 Installing dependencies for ${repoName} 🔨`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) {
-  console.log('☠️❌ Error occured while installing ');
+  console.log('❌ Error occured while installing ❌');
   process.exit(-1);
 }
 
